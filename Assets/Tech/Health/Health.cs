5d1ecebfb9dc047c94efa94ad21c9aff72dc4bf0
fseverijns <frank.severijns.fs@gmail.com>
2020-12-05 13:02:38 +0100

    health clss

---
+++ Assets/Tech/Health/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour {
+  public AgentType agentType;
+  public float maxHealth = 100;
+  private float currentHealth;
+  public event Action<float, float> OnHealthChanged;
+
+  private void Awake() {
+    maxHealth = currentHealth;
+    Healthbar.LinkToHealthbar(this);
+  }
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void TakeDamage(float amount) {
+    currentHealth -= amount;
+    OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+    if (currentHealth <= 0)
+      Kill();
+  }
+
+  public void Kill() {
+    /// kill funct. depends on agent type.
+  }
+}
---
+++ Assets/Tech/UI/Healthbar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ElRaccoone.Tweens;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+  public AgentType agentType;
+  public Health agentHealth;
+  public Image healthbarContainer;
+  public Image healthBarFill;
+  public Color healthbarColorMax;
+  public Color healthbarColorMin;
+  private bool initialized = false;
+
+  public static List<Healthbar> healthbars = new List<Healthbar>();
+
+  public static void LinkToHealthbar(Health health) {
+    foreach (Healthbar hb in healthbars) {
+      if (hb.agentType == health.agentType) {
+        hb.agentHealth = health;
+        hb.Initialize();
+        return;
+      }
+    }
+  }
+
+  private void Awake() {
+    healthbars.Add(this);
+  }
+
+  private void Start() {
+    if (agentHealth != null)
+      Initialize();
+  }
+
+  public void Initialize() {
+    if (initialized)
+      return;
+
+    agentHealth.OnHealthChanged += OnHealthChanged;
+
+    if (initialized) {
+      healthBarFill.TweenImageFillAmount(1, 0.5f).SetFrom(0);
+    }
+  }
+
+  public void OnHealthChanged(float health, float maxHealth) {
+    healthBarFill.TweenImageFillAmount(health / maxHealth, 0.5f);
+    Color barColor = Color.Lerp(healthbarColorMax, healthbarColorMin, health / maxHealth);
+    healthBarFill.TweenGraphicColor(barColor, 0.5f);
+  }
+}
