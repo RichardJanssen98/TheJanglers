b190e0213f1efcd0c0c7b9fe3b63a2a05b751f8c
Richard Janssen <richardjanssen98@gmail.com>
2020-12-05 14:19:48 +0100

    Added lifetime to projectile

--- Assets/Tech/Weapons/Projectile.cs
+++ Assets/Tech/Weapons/Projectile.cs
@@ -7,9 +7,17 @@ public class Projectile : MonoBehaviour
     public float damage;
     public float movementSpeed;
 
+    public float lifeTime = 10;
+
+    private void Start()
+    {
+        GameObject.Destroy(gameObject, lifeTime);
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.up * (movementSpeed * Time.deltaTime);
     }
+
 }
