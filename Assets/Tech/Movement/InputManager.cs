a0c70b748fa8e45781a7d0ffba77ea57b3e3107f
Richard Janssen <richardjanssen98@gmail.com>
2020-12-05 13:02:53 +0100

    Added player movement

---
+++ Assets/Tech/Movement/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public Vector2 MovementPressed()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        return new Vector2(horizontal, vertical);
+    }
+}
---
+++ Assets/Tech/Movement/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private InputManager inputManager;
+
+    Vector2 movementDirection;
+    [SerializeField]
+    private float movementSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        movementDirection = inputManager.MovementPressed();
+        transform.position = transform.position + ((Vector3)movementDirection * (movementSpeed * Time.deltaTime));
+    }
+}
