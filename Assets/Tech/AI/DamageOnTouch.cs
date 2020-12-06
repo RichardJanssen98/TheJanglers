a72bf921d92a7df9bbe71f010bdcc5b36e0e07be
fseverijns <frank.severijns.fs@gmail.com>
2020-12-06 18:06:29 +0100

    enemies damage on touch, player flickering effect

---
+++ Assets/Tech/AI/DamageOnTouch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTouch : MonoBehaviour {
+  public float damageOnTouch = 5;
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    Health health = collision.GetComponent<Health>();
+    if (health != null && health.agentType == AgentType.Player) {
+      health.TakeDamage(damageOnTouch);
+    }
+  }
+}
--- Assets/Tech/Health/Health.cs
+++ Assets/Tech/Health/Health.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using ElRaccoone.Tweens;
 
 public class Health : MonoBehaviour {
   public AgentType agentType;
@@ -9,8 +10,9 @@ public class Health : MonoBehaviour {
   public float currentHealth;
   public event Action<float, float> OnHealthChanged;
   public AudioSource audioSource;
+  public bool invulnerable;
 
-    public AudioClip deathSoundClip;
+  public AudioClip deathSoundClip;
 
   private void Awake() {
     currentHealth = maxHealth;
@@ -25,12 +27,21 @@ public class Health : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-
+    if (Input.GetKeyDown(KeyCode.K)) {
+      TakeDamage(1);
+    }
   }
 
   public void TakeDamage(float amount) {
+    if (invulnerable)
+      return;
     currentHealth -= amount;
     OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+    if (agentType == AgentType.Player) {
+      invulnerable = true;
+      FlickerSprite(0);
+    }
 
     if (currentHealth <= 0)
       Kill();
@@ -51,6 +62,20 @@ public class Health : MonoBehaviour {
         audioSource.PlayOneShot(deathSoundClip);
         break;
     }
+  }
+
+  private void FlickerSprite(float step) {
+    if (step == 20) {
+      invulnerable = false;
+      Player.Instance.movement.charSpriteRenderer.TweenSpriteRendererAlpha(1, 0.1f);
+      return;
+    }
+    float currentAlpha = Player.Instance.movement.charSpriteRenderer.color.a;
+    float nextAlpha = currentAlpha == 1 ? 0 : 1;
+    Player.Instance.movement.charSpriteRenderer.TweenSpriteRendererAlpha(nextAlpha, 0.1f).SetOnComplete(() => {
+      step++;
+      FlickerSprite(step);
+    });
   }
 
   private void OnTriggerEnter2D(Collider2D collision) {
